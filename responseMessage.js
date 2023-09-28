5fbc9db2935426e5df1b1489a882f7b1d7e27eeb
junhee lee <80439077+junheelee411@users.noreply.github.com>
2023-09-28 15:10:16 +0900

    Update responseMessage.js

---
+++ responseMessage.js
@@ -0,0 +1,45 @@
+module.exports = {
+    NULL_VALUE: '필요한 값이 없습니다',
+    OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+    TOO_MUCH_LONG_VALUE: '파라미터 값이 너무 깁니다',
+  
+    // 회원가입
+    CREATED_USER: '회원 가입 성공',
+    DELETE_USER: '회원 탈퇴 성공',
+    ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+    DIFFRERENT_PASSWORD: '처음 입력된 비밀번호와 다릅니다.',
+    SIGNUP_OK: '가입 가능합니다',
+    SIGNUP_NOT_OK: '가입 불가능합니다',
+
+    // 로그인
+    LOGIN_SUCCESS: '로그인 성공',
+    LOGIN_FAIL: '로그인 실패',
+    NO_USER: '존재하지 않는 회원입니다.',
+    MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+    INVALID_EMAIL: '이메일 형식을 확인해주세요.',
+
+    // 권한
+
+    NEED_LOGIN: '단원만 열람 가능합니다.',
+    NEED_AUTHORITY: '권한이 없습니다.',
+
+    // 로그아웃
+    LOGOUT_SUCCESS: '로그아웃 성공',
+    LOGOUT_FAIL: '로그아웃 실패',
+
+    // 파일 업로드
+    ONLY_IMAGE_AVAILABLE: '이미지만 업로드 가능합니다',
+
+    // 서버 내 오류
+    INTERNAL_SERVER_ERROR: '서버 내 오류',
+
+    // 토큰
+    TOKEN_EXPIRED: '토큰이 만료되었습니다.',
+    TOKEN_INVALID: '토큰이 유효하지 않습니다.',
+    TOKEN_EMPTY: '토큰이 없습니다.',
+    NOT_TOKEN_EXPIRED: '토큰이 만료되지 않았습니다.',
+  
+    // 인증
+    NO_AUTH_HEADER: 'Authorization 헤더가 없습니다.',
+    NO_AUTH: '인증되지 않았습니다',
+};
